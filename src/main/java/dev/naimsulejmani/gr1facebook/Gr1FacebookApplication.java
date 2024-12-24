9deea3eaa7b4b28ae8d61953bca3f3589d7d44e0
Naim Sulejmani <naim.sulejmani@gmail.com>
2024-12-24 12:35:22 +0100

    Initial commit

---
+++ src/main/java/dev/naimsulejmani/gr1facebook/Gr1FacebookApplication.java
@@ -0,0 +1,13 @@
+package dev.naimsulejmani.gr1facebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gr1FacebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gr1FacebookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/naimsulejmani/gr1facebook/Gr1FacebookApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.naimsulejmani.gr1facebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gr1FacebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

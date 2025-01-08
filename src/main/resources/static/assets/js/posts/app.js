f5e4e1489804fcba481f16f11d18ebef36b2134b
Naim Sulejmani <naim.sulejmani@gmail.com>
2025-01-08 13:26:10 +0100

    a mundesh edhe nihere push tlutna

--- src/main/resources/static/assets/js/posts/app.js
+++ src/main/resources/static/assets/js/posts/app.js
@@ -1,11 +1,74 @@
 // STARTIMI - apo inicializimi i programit
 // lidhja e DOM me variablat ne JS
 
-const postApi = new PostApi();
+// const postApi = new PostApi();
+//
+// postApi.findById(3)
+//     .then(data => console.log("Loaded successfully!"))
+//     .catch(error => console.error("Error loading data!"))
+//     .finally(() => console.log("I am done!"));
+//
+// console.log("CKA BONE NJERI!");
 
-postApi.findAll()
-    .then(data => console.log("Loaded successfully!"))
-    .catch(error => console.error("Error loading data!"))
-    .finally(() => console.log("I am done!"));
+class PostApp {
+    constructor() {
+        this.postApi = new PostApi();
+        this.posts = [];
+        this.postsTBody = document.getElementById("posts");
+        this.loaderDiv = document.getElementById("loader");
+        this.mainTable = document.querySelector("div.table-responsive");
+    }
 
-console.log("CKA BONE NJERI!");
+    async init() {
+        this.mainTable.style = {display: "none"};
+        this.loaderDiv.style.display = "block";
+
+        this.posts = await this.postApi.findAll();
+        this.displayPosts();
+
+        this.mainTable.style = {display: "block"}
+        this.loaderDiv.style.display = "none";
+        // setTimeout(() => {
+        //     this.loaderDiv.style = "display:none";
+        //     this.mainTable.style = "display:block";
+        // }, 2000)
+    }
+
+    displayPosts() {
+        this.postsTBody.innerHTML = '';
+
+        this.posts.forEach(post => {
+            this.postsTBody.innerHTML += `
+                <tr>
+                    <td>${post.id}</td>
+                    <td>${post.userId}</td>
+                    <td>${post.content}</td>
+                    <td>${post.createdAt}</td>
+                    <td>
+                        <button class="btn btn-danger" type="button">DELETE</button>
+                    </td>
+                </tr>
+            `;
+        });
+    }
+}
+
+new PostApp().init()
+    .then(() => console.log("Application initialized successfully!"))
+    .catch(error => console.error("Error initializing application!"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
